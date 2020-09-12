c5538c3aa2ce283898827abab662c6ea2604f77f
christiankozalla <christian.kozalla@gmail.com>
2020-09-12 22:30:38 +0200

    Google Analytics running

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,7 @@
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import * as gtag from '../lib/gtag';
+
 import { SWRConfig } from 'swr';
 import fetch from '../lib/fetch';
 import { DefaultSeo } from 'next-seo';
@@ -6,11 +10,23 @@ import SEO from '../next-seo.config';
 import Container from '../components/Container';
 import '../styles/global.css';
 
-export default ({ Component, pageProps }) => (
-  <SWRConfig value={{ fetcher: fetch }}>
-    <Container>
-      <DefaultSeo {...SEO} />
-      <Component {...pageProps} />
-    </Container>
-  </SWRConfig>
-);
+export default ({ Component, pageProps }) => {
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+  return (
+    <SWRConfig value={{ fetcher: fetch }}>
+      <Container>
+        <DefaultSeo {...SEO} />
+        <Component {...pageProps} />
+      </Container>
+    </SWRConfig>
+  );
+};
--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,7 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 
+import { GA_TRACKING_ID } from '../lib/gtag';
+
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
     const initialProps = await Document.getInitialProps(ctx);
@@ -12,6 +14,23 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
+          {/* Global Site Tag (gtag.js) - Google Analytics */}
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `
+            }}
+          />
           <meta content="IE=edge" httpEquiv="X-UA-Compatible" />
           <meta content="width=device-width, initial-scale=1" name="viewport" />
           <link
