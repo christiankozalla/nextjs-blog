15925dddf26c2e246394318c6c5b43248d2f7adc
christiankozalla <christian.kozalla@gmail.com>
2021-01-20 08:50:52 +0100

    Uses Preact for production client-side bundle bump packages to latest

--- next.config.js
+++ next.config.js
@@ -24,5 +24,17 @@ module.exports = {
         ]
       }
     ];
+  },
+  webpack: (config, { dev, isServer }) => {
+    // Replace React with Preact for production client-side bundle
+    if (!dev && !isServer) {
+      Object.assign(config.resolve.alias, {
+        react: 'preact/compat',
+        'react-dom/test-utils': 'preact/test-utils',
+        'react-dom': 'preact/compat'
+      });
+    }
+
+    return config;
   }
 };
